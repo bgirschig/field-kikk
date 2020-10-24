a97c78d00692a22b850374f92dffce59ff854f76
bastien girschig <bastien.girschig@gmail.com>
2020-10-24 19:22:30 +0200

    cleanup

--- Assets/scripts/messageProtos/SwingAction.cs
+++
@@ -1,8 +0,0 @@
-// used by the control room to send commands "down" to a swing instance
-namespace MessageProtos {
-  class SwingControl {
-    public string messageType = "SwingControl";
-    public int swing_id;
-    public string action;
-  }
-}
